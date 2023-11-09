05c34eabbf23ae88a43706f9cfb7f7e9dd6764c0
Kashiwazaki Seiichirou <s.kashiwazaki.sys23@morijyobi.ac.jp>
2023-11-09 15:38:13 +0900

    Main.javaの作成（コミットメッセージ修正）
    BMIの計算を一回の実行で二人分できるように変更しました。
    直前のコミットメッセージを修正しました。

--- Main.java
+++ Main.java
@@ -1,26 +1,43 @@
 import java.util.Scanner;
 
-public class Main {
+public class a {
     public static void main(String[] args){
         System.out.println("BMIの計算をします。");
-        System.out.println("体重と身長を入力してください。");
+        System.out.println("二人分の体重と身長を入力してください。");
         Scanner sc = new Scanner(System.in);
-        System.out.println("体重(kg)");
+        System.out.println("一人目の体重(kg)");
         double weight = sc.nextDouble();
-        System.out.println("身長(m)");
+        System.out.println("一人目の身長(m)");
         double height = sc.nextDouble();
+        System.out.println("二人目の体重(kg)");
+        double weight2 = sc.nextDouble();
+        System.out.println("二人目の身長(m)");
+        double height2 = sc.nextDouble();
+
 
         double bmi = weight / (height * height);
-        System.out.println("BMI:" + String.format("%.2f", bmi));
+        System.out.println("BMI(一人目):" + String.format("%.2f", bmi));
+        double bmi2 = weight2 / (height2 * height2);
+        System.out.println("BMI(二人目):" + String.format("%.2f", bmi2));
 
         if(bmi < 18.5) {
-            System.out.println("評価：低体重");
+            System.out.println("一人目の評価：低体重");
 
         }else if(bmi < 25) {
-            System.out.println("評価：普通体重");
+            System.out.println("一人目の評価：普通体重");
 
         }else {
-            System.out.println("評価：肥満");
+            System.out.println("一人目の評価：肥満");
+        }
+
+        if(bmi2 < 18.5) {
+            System.out.println("二人目の評価：低体重");
+
+        }else if(bmi2 < 25) {
+            System.out.println("二人目の評価：普通体重");
+
+        }else {
+            System.out.println("二人目の評価：肥満");
         }
 
 
